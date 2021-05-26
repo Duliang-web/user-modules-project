b5fff86b9c547d60a369f2741c2246c3fbe2372a
lijinze6 <lijinze6@jd.com>
2021-05-26 11:26:21 +0800

    feat: 更新

---
+++ user-hessian-module/src/hessian.java
@@ -0,0 +1,5 @@
+/**
+ * Created by lijinze6 on 2021/5/26.
+ */
+public class hessian {
+}
---
+++ user-test-module/src/test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lijinze6 on 2021/5/26.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
